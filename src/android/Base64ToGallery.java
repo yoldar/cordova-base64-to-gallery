9133330e2f9ff706d2cb66bd7a0c2edefac2960b
Jason He <zhujin001xb@163.com>
2020-03-04 13:50:55 +0800

    Update Base64ToGallery.java
    增加权限动态获取

--- src/android/Base64ToGallery.java
+++ src/android/Base64ToGallery.java
@@ -7,10 +7,12 @@ import java.util.Calendar;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
+import org.apache.cordova.PermissionHelper;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -18,6 +20,8 @@ import android.os.Build;
 import android.os.Environment;
 import android.util.Base64;
 import android.util.Log;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 /**
  * Base64ToGallery.java
@@ -32,7 +36,10 @@ public class Base64ToGallery extends CordovaPlugin {
 
   // Consts
   public static final String EMPTY_STR = "";
-
+  private CallbackContext _callback;
+  private Bitmap _bmp;
+  private String _filePrefix;
+  private boolean _mediaScannerEnabled;
   @Override
   public boolean execute(String action, JSONArray args,
       CallbackContext callbackContext) throws JSONException {
@@ -40,7 +47,9 @@ public class Base64ToGallery extends CordovaPlugin {
     String base64               = args.optString(0);
     String filePrefix           = args.optString(1);
     boolean mediaScannerEnabled = args.optBoolean(2);
-
+    this._callback = callbackContext;
+    this._mediaScannerEnabled = mediaScannerEnabled;
+    this._filePrefix = filePrefix;
     // isEmpty() requires API level 9
     if (base64.equals(EMPTY_STR)) {
       callbackContext.error("Missing base64 string");
@@ -54,27 +63,22 @@ public class Base64ToGallery extends CordovaPlugin {
       callbackContext.error("The image could not be decoded");
 
     } else {
+      this._bmp = bmp;
 
-      // Save the image
-      File imageFile = savePhoto(bmp, filePrefix);
+      if (PermissionHelper.hasPermission(this, WRITE_EXTERNAL_STORAGE)) {
+        Log.d("SaveImageGallery", "Permissions already granted, or Android version is lower than 6");
 
-      if (imageFile == null) {
-        callbackContext.error("Error while saving image");
+        savePhoto(bmp, filePrefix, this._callback);
+      } else {
+        Log.d("SaveImageGallery", "Requesting permissions for WRITE_EXTERNAL_STORAGE");
+        PermissionHelper.requestPermission(this, 1000, WRITE_EXTERNAL_STORAGE);
       }
-
-      // Update image gallery
-      if (mediaScannerEnabled) {
-        scanPhoto(imageFile);
-      }
-
-      callbackContext.success(imageFile.toString());
     }
 
     return true;
   }
 
-  private File savePhoto(Bitmap bmp, String prefix) {
-    File retVal = null;
+  private void savePhoto(Bitmap bmp, String prefix ,CallbackContext callbackContext) {
 
     try {
       String deviceVersion = Build.VERSION.RELEASE;
@@ -107,20 +111,30 @@ public class Base64ToGallery extends CordovaPlugin {
         folder = Environment.getExternalStorageDirectory();
       }
 
-      File imageFile = new File(folder, prefix + date + ".jpg");
+      File imageFile = new File(folder, prefix + date + ".png");
 
       FileOutputStream out = new FileOutputStream(imageFile);
-      bmp.compress(Bitmap.CompressFormat.JPEG, 100, out);
+      bmp.compress(Bitmap.CompressFormat.PNG, 100, out);
       out.flush();
       out.close();
 
-      retVal = imageFile;
+      if (imageFile == null) {
+        callbackContext.error("Error while saving image");
+      }
+
+      // Update image gallery
+      if (this._mediaScannerEnabled) {
+        scanPhoto(imageFile);
+      }
+
+      callbackContext.success(imageFile.toString());
+
 
     } catch (Exception e) {
       Log.e("Base64ToGallery", "An exception occured while saving image: " + e.toString());
+      callbackContext.error("An exception occured while saving image: " + e.toString());
     }
 
-    return retVal;
   }
 
   /**
@@ -134,5 +148,22 @@ public class Base64ToGallery extends CordovaPlugin {
     mediaScanIntent.setData(contentUri);
 
     cordova.getActivity().sendBroadcast(mediaScanIntent);
+  }
+
+  public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+    for (int r : grantResults) {
+      if (r == PackageManager.PERMISSION_DENIED) {
+        Log.d("SaveImageGallery", "Permission not granted by the user");
+        _callback.error("Permissions denied");
+        return;
+      }
+    }
+
+    switch (requestCode) {
+      case 1000:
+        Log.d("SaveImageGallery", "User granted the permission for WRITE_EXTERNAL_STORAGE");
+        savePhoto(this._bmp, this._filePrefix, this._callback);
+        break;
+    }
   }
 }
